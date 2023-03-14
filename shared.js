30bee0ca980f4b531c25bea4c1326868411073d0
Matthew de Marillac <53460562+mattdemarillac@users.noreply.github.com>
2023-03-14 16:21:42 +1000

    Update shared.js

--- shared.js
+++ shared.js
@@ -1,12 +1,18 @@
-// Best to keep the default setup configuration by leaving this in the root folder, we don't want webpack bundling service workers by mistake -
-// this could cause some issues.
+const allPorts = [];
 
-onconnect = (e) => {
-    const port = e.ports[0]
+onconnect = function(e) {
+  // the incoming port
+  var port = e.ports[0];
+  allPorts.push(port);
 
-    port.addEventListener("message", (e) => {
-        port.postMessage(e.data)
+  port.addEventListener('message', function(e) {
+    // get the message sent to the worker
+    var message = e.data[0];
+    // send the message to ALL connected worker ports!
+    allPorts.forEach(port => {
+      port.postMessage(message);
     })
+  });
 
-    port.start() // Required when using addEventListener. Otherwise called implicitly by onmessage setter.
+  port.start(); // Required when using addEventListener. Otherwise called implicitly by onmessage setter.
 }
